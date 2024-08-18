336d7bb187b95aa849c971fc0a7b5faf43c9065e
PlumbumChoki <kirkagrit@gmail.com>
2024-08-18 12:53:24 +0300

    Add files via upload

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,7 @@
+﻿using HW1;
+using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern int MessageBox(IntPtr hWnd, String text, String caption, int options);
+MessageBox(IntPtr.Zero, "Next message box will be with information", "My Message", 0);
+MessageBox(IntPtr.Zero, new Student("Алексей", "Орлов", new DateTime (1990,10,10)).ToString(), "My Message", 0);
---
+++ HW1/HW1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }        
+        public DateTime BD { get; set; }
+        public int Age { get { return DateTime.Now.Year - BD.Year; } }
+
+        public Student(string firstName, string lastName, DateTime bd)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BD = bd;
+        }
+
+        public override string ToString()//перегрузка класса object - базовый класс для всех классов
+        {
+            return $"{FirstName,-20} {LastName,-20} {{BD.ToShortDateString(),-20}} {{Age,-5";
+        }
+
+
+         
+    }
+}
