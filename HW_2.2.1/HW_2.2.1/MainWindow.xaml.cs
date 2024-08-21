272108a5810c53bdc995261e2c17f1bee65c4758
PlumbumChoki <kirkagrit@gmail.com>
2024-08-21 11:35:19 +0300

    Add files via upload

---
+++ HW_2.2.1/HW_2.2.1/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_2._2._1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Process? process;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            process = Process.Start("C:\\Учёба\\Системное программирование\\HW_2.2\\HW_2.2\\bin\\Debug\\net8.0-windows\\HW_2.2.exe");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            process?.Kill();
+            process = null;
+        }
+    }
+}
